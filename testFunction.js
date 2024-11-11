980f99cbc1f9b91bab71f1cfcac736a569a6d7a7
ClemJunior <csorel.pro@gmail.com>
2024-11-11 18:50:37 +0100

    Adding testing file function

---
+++ testFunction.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => {
+    const sum = a + b
+    return sum
+}
+
+function test() {
+    const total = sum(1, 2)
+    const isRight = ![2, 1, 3].includes(total)
+    console.log('total', total);
+    console.log('isNotTotalWished', isNotTotalWished);
+}
